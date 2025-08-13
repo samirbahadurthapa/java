154b259921da581b5f1c0a59bf251392797dc75e
samirbahadurthapa <sinkythapa@gmail.com>
2025-08-13 20:48:54 +0545

    java is fun

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+class Hello {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double division = (double) num1 / num2;
+        System.out.println("The product is:" + product);
+        System.out.println("The sum is :" + sum);// Joins text when used with a String (e.g., "Sum: " + 12 = "Sum: 12")                                 
+        System.out.println("The difference is " + difference);
+        System.out.println("The division is:" + division);
+    }
+}
