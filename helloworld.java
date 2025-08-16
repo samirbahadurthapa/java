1c4a240e6d362842abbe8978ffc91d47a3c9a107
samirbahadurthapa <sinkythapa@gmail.com>
2025-08-16 13:31:44 +0545

    feat:added advanced calculator

--- helloworld.java
+++ helloworld.java
@@ -5,10 +5,13 @@ class Hello {
         int sum = num1 + num2;
         int difference = num1 - num2;
         int product = num1 * num2;
-        double division = (double) num1 / num2;
+        double division = (double) num1 / num2;  
+        int quotient=num1/num2;
+
         System.out.println("The product is:" + product);
         System.out.println("The sum is :" + sum);// Joins text when used with a String (e.g., "Sum: " + 12 = "Sum: 12")                                 
         System.out.println("The difference is " + difference);
-        System.out.println("The division is:" + division);
+        System.out.println("The division is:" + division); 
+        System.out.println("The quotient is:"+quotient);
     }
 }
